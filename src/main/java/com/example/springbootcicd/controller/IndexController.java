280be315ff5699db561d8d8d531c88cb7623564a
JuHyun419 <zzang9ha@naver.com>
2021-08-02 14:33:58 +0900

    Initial commit

---
+++ src/main/java/com/example/springbootcicd/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.example.springbootcicd.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping(value = {"/index", "/"})
+    public String index() {
+        return "index";
+    }
+}
---
+++ src/test/java/com/example/springbootcicd/SpringBootCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCicdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
