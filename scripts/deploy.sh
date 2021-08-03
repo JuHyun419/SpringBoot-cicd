bc8825f3c82106c23fd074b7994c4ca7a53e4ee7
JuHyun419 <zzang9ha@naver.com>
2021-08-03 12:52:33 +0900

    Test: Jenkins, CodeDeploy Test23

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -21,4 +21,5 @@ fi
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo "> DEPLOY_JAR 배포" # /home/ec2-user/deploy.log
-nohup java -jar $DEPLOY_JAR /dev/null 2>&1 &
+#nohup java -jar $DEPLOY_JAR /dev/null 2>&1 &
+nohup java -jar $DEPLOY_JAR 2>/home/ec2-user/deploy_err.log &
